fb00375a4573578a63194e188e975029533e9c1a
Rahmans11 <148077278+Rahmans11@users.noreply.github.com>
2025-11-11 09:21:24 +0700

    update mini task 12
    update fetching data with lowercase method & async await form

--- minitask12.js
+++ minitask12.js
@@ -13,29 +13,57 @@ fetch(url)
         let i = 0
         let emailArr = [];
         let email = [];
-        let emailForm = [];
-        let lowerCaseEmail = "";
+        let lowerCaseEmail = [];
+        let charCode = "";
         for(;i < body.length; i++){
             emailArr = [...emailArr,body[i].email];
             email.push(emailArr[i])
-            for (let j = 0; j < email[i].length; j++) {
-                let charCode = email[i].charCodeAt(j);
+            lowerCaseEmail = email.map(element => {
+                for (let j = 0; j < email[i].length; j++) {
                 if (charCode >= 65 && charCode <= 90) {
                 lowerCaseEmail += String.fromCharCode(charCode + 32);
                 } else {
                 lowerCaseEmail += email[i].charAt(j);
                 }
-                
             }
-            emailForm.push(lowerCaseEmail);
+            return element.lowerCaseEmail;
+            });
         }
-        //emailForm.push(lowerCaseEmail);
-        console.log(emailForm);
+
+        console.log(lowerCaseEmail);
         
     })
     .catch((err)=>console.log(err.message));
 
-async function funcFetch() {
+fetch(url)
+    .then((result)=>{
+        if(!result.ok){
+            throw new Error(`${result.status}`
+                `${result.statusText}`
+            );
+        }
+        return result.json();
+    })
+    .then((body)=>{
+        let i = 0
+        let emailArr = [];
+        let email = [];
+        let lowerCaseEmail = [];
+        for(;i < body.length; i++){
+            emailArr = [...emailArr,body[i].email];
+            email.push(emailArr[i])
+            lowerCaseEmail = email.map(element => {
+            return element.toLowerCase();
+            });
+            
+        }
+
+        console.log(lowerCaseEmail);
+        
+    })
+    .catch((err)=>console.log(err.message));
+
+async function funcFetch(url) {
     try{
         let result = await fetch(url);
         if (!result.ok) {
@@ -45,12 +73,21 @@ async function funcFetch() {
         }
         let body = await result.json();
         let i = 0
+        let emailArr = [];
         let email = [];
+        let lowerCaseEmail = [];
         for(;i < body.length; i++){
-            email = [...email,body[i].email];
-            console.log([body[i].email]);
+            emailArr = [...emailArr,body[i].email];
+            email.push(emailArr[i])
+            lowerCaseEmail = email.map(element => {
+            return element.toLowerCase();
+            });
+            
         }
+        return console.log(lowerCaseEmail);;
         }catch(error){
         console.log(err.message);
+        throw error;
     }
 }
+funcFetch(url);
